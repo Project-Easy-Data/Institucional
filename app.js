a6d9b088999d32c10f23f6bfea87f9c7911beab0
JoaoGomes-Melo <joao.gmelo@sptech.school>
2026-05-21 18:15:06 -0300

    Atualizando rag

--- app.js
+++ app.js
@@ -17,15 +17,21 @@ var app = express();
 
 var indexRouter = require("./src/routes/index");
 var usuariosRouter = require("./src/routes/usuarios");
+var funcionariosRouter = require("./src/routes/funcionarios");
+var deletarRouter = require("./src/routes/deletar");
+var ragRouter = require("./src/routes/rag");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/rag", ragRouter);
 
 app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/usuarios", usuariosRouter);
+app.use("/funcionarios", funcionariosRouter);
+app.use("/deletar", deletarRouter);
 
 app.listen(PORTA_APP, function () {
     console.log(`
