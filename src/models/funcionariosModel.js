dae4559b2802cb10e4b3658be510687ba1e2c013
rafaelP-calderon <rafael.calderon@sptech.school>
2026-06-07 14:41:32 -0300

    Correções feitas em model, controller e route de Funcionário

--- src/models/funcionariosModel.js
+++ src/models/funcionariosModel.js
@@ -1,40 +1,62 @@
-var database = require("../database/config")
+var funcionariosModel = require("../models/funcionariosModel");
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
-function cadastrar(nome, email, senha, permissao, cargo, empresaId) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, cargo, permissao, empresaId);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
-    var instrucaoSql = `
-    INSERT INTO Usuario (nome, email, senha, fk_Cargo, permissao, status, fk_Empresa) 
-    VALUES ('${nome}', '${email}', '${senha}', ${cargo}, ${permissao}, 'ATIVO', ${empresaId});
-`;
+function cadastrar(req, res) {
+    var nome = req.body.nomeServer
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+    var permissao = req.body.permissaoServer;
+    var cargo = req.body.cargoServer;
+    var empresaId = 1;
 
-    
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+    if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está indefinida!");
+    } else if (nome == undefined) {
+        res.status(400).send("Seu nome está indefinido!");
+    } else if (permissao == undefined) {
+        res.status(400).send("Sua permissao está indefinida!");
+    } else if (cargo == undefined) {
+        res.status(400).send("Seu cargo está indefinido!");
+    } else {
+        funcionariosModel.cadastrar(nome, email, senha, cargo, permissao, empresaId)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
 }
 
-function listar(empresaId) {
-    var instrucaoSql = `
-        SELECT u.id_funcionario, u.nome, u.email, c.cargo
-        FROM Usuario u
-        INNER JOIN Cargo c ON u.fk_Cargo = c.idCargo
-        WHERE u.fk_Empresa = ${empresaId};
-    `;
-    return database.executar(instrucaoSql);
+function listar(req, res) {
+    funcionariosModel.listar(1)
+        .then(function(resultado) {
+            res.json(resultado);
+        })
+        .catch(function(erro) {
+            res.status(500).json(erro.sqlMessage);
+        });
+}
+
+function excluir(req, res) {
+    var id = req.params.id;
+    funcionariosModel.excluir(id)
+        .then(function(resultado) {
+            res.json(resultado);
+        })
+        .catch(function(erro) {
+            console.log("\nHouve um erro ao excluir! Erro: ", erro.sqlMessage);
+            res.status(500).json(erro.sqlMessage);
+        });
 }
 
 module.exports = { cadastrar, excluir, listar };
-
-function excluir(id) {
-    var instrucaoSql = `DELETE FROM Usuario WHERE id_funcionario = ${id};`;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
-module.exports = {
-    cadastrar,
-    excluir
-};
--- src/routes/funcionarios.js
+++ src/routes/funcionarios.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var funcionariosController = require("../controllers/funcionariosController");
 
-//Recebendo os dados do html e direcionando para a função cadastrar de usuarioController.js
 router.post("/cadastrar", function (req, res) {
     funcionariosController.cadastrar(req, res);
 })
