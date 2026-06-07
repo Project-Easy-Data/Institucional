39f87de92b1b02495e34999400aa030aafd8e8bd
rafaelP-calderon <rafael.calderon@sptech.school>
2026-06-07 14:47:30 -0300

    Model e Controller funcionando

--- src/controllers/funcionariosController.js
+++ src/controllers/funcionariosController.js
@@ -1,29 +1,41 @@
-var database = require("../database/config")
+var funcionariosModel = require("../models/funcionariosModel");
 
-function cadastrar(nome, email, senha, permissao, cargo, empresaId) {
-    var instrucaoSql = `
-        INSERT INTO Usuario (nome, email, senha, fk_Cargo, permissao, status, fk_Empresa)
-        VALUES ('${nome}', '${email}', '${senha}', ${cargo}, ${permissao}, 'ATIVO', ${empresaId});
-    `;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+function cadastrar(req, res) {
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
+    var permissao = req.body.permissaoServer;
+    var cargo = req.body.cargoServer;
+    var empresaId = 1;
+
+    if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está indefinida!");
+    } else if (nome == undefined) {
+        res.status(400).send("Seu nome está indefinido!");
+    } else if (permissao == undefined) {
+        res.status(400).send("Sua permissao está indefinida!");
+    } else if (cargo == undefined) {
+        res.status(400).send("Seu cargo está indefinido!");
+    } else {
+        funcionariosModel.cadastrar(nome, email, senha, cargo, permissao, empresaId)
+            .then(function(resultado) { res.json(resultado); })
+            .catch(function(erro) { res.status(500).json(erro.sqlMessage); });
+    }
 }
 
-function listar(empresaId) {
-    var instrucaoSql = `
-        SELECT u.id_funcionario, u.nome, u.email, c.cargo
-        FROM Usuario u
-        INNER JOIN Cargo c ON u.fk_Cargo = c.idCargo
-        WHERE u.fk_Empresa = ${empresaId};
-    `;
-    return database.executar(instrucaoSql);
+function listar(req, res) {
+    funcionariosModel.listar(1)
+        .then(function(resultado) { res.json(resultado); })
+        .catch(function(erro) { res.status(500).json(erro.sqlMessage); });
 }
 
-function excluir(id) {
-    var instrucaoSql = `DELETE FROM Usuario WHERE id_funcionario = ${id};`;
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
+function excluir(req, res) {
+    var id = req.params.id;
+    funcionariosModel.excluir(id)
+        .then(function(resultado) { res.json(resultado); })
+        .catch(function(erro) { res.status(500).json(erro.sqlMessage); });
 }
 
 module.exports = { cadastrar, excluir, listar };
-
